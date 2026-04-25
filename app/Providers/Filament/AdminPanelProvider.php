0b3d51486063f956f645d0c767ea93b05509ac7a
mohamad jalim <mohamadjalim05@gmail.com>
2026-04-25 12:39:01 +0100

    modifi login page

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('SaharianCamp')
             ->profile()
             ->colors([
                 'primary' => Color::Amber,
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 Route::get('/init-db', function () {
